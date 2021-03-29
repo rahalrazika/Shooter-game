9fd30d60dc4433db4a236b04b89cddcea50b1e64
rahalrazika <rahalraika@gmail.com>
2021-03-29 19:44:02 +0200

    change the logo in the first page

--- src/scenes/playerName.js
+++ src/scenes/playerName.js
@@ -10,16 +10,16 @@ export default class PlayerName extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('namelogo', '../src/assets/namelogo.jpg');
+    this.load.image('namelogo', '../src/assets/namelogo.png');
   }
 
   create() {
     this.cameras.main.setBackgroundColor(0xE8E8E8);
     this.logo = this.add.image(350, 70, 'namelogo');
     this.logo.setScale(0.7);
-    this.text = this.add.text(120, 170, 'Please enter your player name:', { fontSize: '20px', fill: '#00000' });
-    const input = this.add.dom(300, 220, 'input', 'background-color: white; width: 220px; height: 50px; font: 22px Arial');
-    this.button = this.add.dom(300, 300, 'button', 'background-color: grey; width: 120px; height: 50px; font: 22px Arial', 'Submit');
+    this.text = this.add.text(150, 170, 'Please enter your player name:', { fontSize: '20px', fill: '#00000' });
+    const input = this.add.dom(350, 220, 'input', 'background-color: white; width: 220px; height: 50px; font: 22px Arial');
+    this.button = this.add.dom(350, 300, 'button', 'background-color: grey; width: 120px; height: 50px; font: 22px Arial', 'Submit');
 
     const submit = document.querySelector('button');
 
