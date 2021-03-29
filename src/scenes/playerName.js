2de922f01bc9a5b4b7ed8de5f32d6b1215a37b0d
rahalrazika <rahalraika@gmail.com>
2021-03-29 20:10:58 +0200

    remove the logo image from the playername scene

--- src/scenes/playerName.js
+++ src/scenes/playerName.js
@@ -9,17 +9,12 @@ export default class PlayerName extends Phaser.Scene {
     this.userName = data.user;
   }
 
-  preload() {
-    this.load.image('namelogo', '../src/assets/namelogo.png');
-  }
-
   create() {
-    this.cameras.main.setBackgroundColor(0xE8E8E8);
-    this.logo = this.add.image(350, 70, 'namelogo');
-    this.logo.setScale(0.7);
-    this.text = this.add.text(150, 170, 'Please enter your player name:', { fontSize: '20px', fill: '#00000' });
-    const input = this.add.dom(350, 220, 'input', 'background-color: white; width: 220px; height: 50px; font: 22px Arial');
-    this.button = this.add.dom(350, 300, 'button', 'background-color: grey; width: 120px; height: 50px; font: 22px Arial', 'Submit');
+    this.cameras.main.setBackgroundColor(0x000000);
+   
+    this.text = this.add.text(150, 50, 'Please enter your player name:', { fontSize: '20px', fill: '#FFFFFF' });
+    const input = this.add.dom(320, 120, 'input', 'background-color: white; width: 220px; height: 50px; font: 22px Arial');
+    this.button = this.add.dom(320, 200, 'button', 'background-color: #89CFF0; width: 120px; height: 50px; font: 22px Arial', 'Submit');
 
     const submit = document.querySelector('button');
 
